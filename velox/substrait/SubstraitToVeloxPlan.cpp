b1103661e9998baa3705de15ec6b86261cbd38c1
donghekang <kangdonghe.k@gmail.com>
2022-12-29 13:25:20 -0500

    support substrait roundrobin exchange

--- velox/substrait/SubstraitToVeloxPlan.cpp
+++ velox/substrait/SubstraitToVeloxPlan.cpp
@@ -16,6 +16,7 @@
 
 #include "velox/substrait/SubstraitToVeloxPlan.h"
 #include "velox/exec/HashPartitionFunction.h"
+#include "velox/exec/RoundRobinPartitionFunction.h"
 #include "velox/substrait/TypeUtils.h"
 #include "velox/substrait/VariantToVectorConverter.h"
 #include "velox/type/Type.h"
@@ -293,32 +294,44 @@ core::PlanNodePtr SubstraitVeloxPlanConverter::toVeloxPlan(
 
 core::PlanNodePtr SubstraitVeloxPlanConverter::toVeloxPlan(
     const ::substrait::ExchangeRel& exchangeRel) {
-  if (exchangeRel.exchange_kind_case() !=
-      ::substrait::ExchangeRel::kScatterByFields)
-    VELOX_UNSUPPORTED(
-        "Substrait convert does not support {} ExchangeRel",
-        exchangeRel.exchange_kind_case());
   core::PlanNodePtr childNode;
   if (exchangeRel.has_input())
     childNode = toVeloxPlan(exchangeRel.input());
   else
     VELOX_FAIL("Child Rel is expected in ExchangeRel.");
 
-  const auto& inputType = childNode->outputType();
-  std::vector<column_index_t> keyChannels;
-  for (const auto& field : exchangeRel.scatter_by_fields().fields()) {
-    const auto expr = exprConverter_->toVeloxExpr(field, inputType);
-    keyChannels.push_back(inputType->getChildIdx(expr->name()));
-  }
+  core::PartitionFunctionFactory factory;
+  if (exchangeRel.exchange_kind_case() ==
+      ::substrait::ExchangeRel::kScatterByFields) {
+    const auto& inputType = childNode->outputType();
+    std::vector<column_index_t> keyChannels;
+    for (const auto& field : exchangeRel.scatter_by_fields().fields()) {
+      const auto expr = exprConverter_->toVeloxExpr(field, inputType);
+      keyChannels.push_back(inputType->getChildIdx(expr->name()));
+    }
+
+    factory =
+        [inputType, keyChannels](
+            int numPartitions) -> std::unique_ptr<core::PartitionFunction> {
+      return std::make_unique<exec::HashPartitionFunction>(
+          numPartitions, inputType, keyChannels);
+    };
+  } else if (
+      exchangeRel.exchange_kind_case() ==
+      ::substrait::ExchangeRel::kRoundRobin) {
+    factory =
+        [](int numPartitions) -> std::unique_ptr<core::PartitionFunction> {
+      return std::make_unique<exec::RoundRobinPartitionFunction>(numPartitions);
+    };
+  } else
+    VELOX_UNSUPPORTED(
+        "Substrait convert does not support {} ExchangeRel",
+        exchangeRel.exchange_kind_case());
 
   return std::make_shared<core::LocalPartitionNode>(
       nextPlanNodeId(),
       core::LocalPartitionNode::Type::kRepartition,
-      [inputType, keyChannels](
-          int numPartitions) -> std::unique_ptr<core::PartitionFunction> {
-        return std::make_unique<exec::HashPartitionFunction>(
-            numPartitions, inputType, keyChannels);
-      },
+      factory,
       std::vector<core::PlanNodePtr>{childNode});
 }
 
